a5592879c6a48735ff5c449441f297dbb6fb6f25
SchadiaT <84747674+SchadiaT@users.noreply.github.com>
2021-05-25 00:02:31 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+app.js
+
+var constraints = { video: {facingMode: "user" }, audio: true };
+
+const cameraView = document.querySelector ("#camera--view"),
+      cameraOutput = document.querySelector ("#")
